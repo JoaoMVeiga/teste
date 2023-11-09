6db347f125d1425788abec1762310257358e1b15
João Veiga <67605388+JoaoMVeiga@users.noreply.github.com>
2023-11-09 22:54:11 +0000

    Update connection.php

--- api/connection.php
+++ api/connection.php
@@ -4,22 +4,25 @@ $username = "x47dpkdo8h444vcllq6e";
 $password = "pscale_pw_Sbwpx17SXysWnInCjDzvRhCEf8nOoCZZznT6SiMAYjZ";
 $dbname = "mariananailsatrist";
 
-// Create connection
+// Criar conexão
 $conn = new mysqli($servername, $username, $password, $dbname, null, null);
 
-// Check connection
+// Verificar conexão
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    die("Falha na conexão: " . $conn->connect_error);
 }
 
-// Additional SSL/TLS configuration
-$conn->ssl_set(null, null, '/etc/pki/tls/certs/ca-bundle.crt', null, null);
+// Adicionar variável de sistema PLANETSCALE_SSL_CERT_PATH ao caminho do certificado SSL
+$ssl_cert_path = getenv('PLANETSCALE_SSL_CERT_PATH');
+if ($ssl_cert_path) {
+    $conn->ssl_set(null, null, $ssl_cert_path, null, null);
+}
 
-// Check if SSL configuration is successful
+// Verificar se a configuração SSL foi bem-sucedida
 if (!$conn->real_connect($servername, $username, $password, $dbname)) {
-    die("Connection failed: " . $conn->connect_error);
+    die("Falha na conexão: " . $conn->connect_error);
 }
 
-// Rest of your code...
+// Restante do seu código...
 
 ?>
