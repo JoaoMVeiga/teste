72e1dffe3fa67f255f6523b96ed0ef150803ef1c
João Veiga <67605388+JoaoMVeiga@users.noreply.github.com>
2023-11-09 15:53:45 +0000

    Add files via upload

---
+++ api/controller/connection.php
@@ -0,0 +1,21 @@
+<?php
+$servername = "aws.connect.psdb.cloud";
+$username = "hre4whbsult1zijifvyg";
+$password = "pscale_pw_MwKASCmwCTsRHSbB7yjxAe9Cw9mjoot1SdBXiEhibbq"; //2gUeCUsOYbfjn5Iprcwo
+$dbname = "mariananailsatrist";
+//2gUeCUsOYbfjn5Iprcwo
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+?>
+
+DB_HOST=
+DB_USERNAME=05nsos54qd07qfv8b0qg
+DB_PASSWORD=************
+DB_NAME=mariananailsatrist
+  
