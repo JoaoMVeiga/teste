a0eaea5c01d15b82c25429477949d675a62a31a3
João Veiga <67605388+JoaoMVeiga@users.noreply.github.com>
2023-11-09 18:20:54 +0000

    Update connection.php

--- api/connection.php
+++ api/connection.php
@@ -4,23 +4,11 @@ $username = "q22jl9a79rsymx497jfd";
 $password = "pscale_pw_86DHhEdyEiF8LWxAM8dPmNcpo4kByYGSJZXLhkHmGkA";
 $dbname = "mariananailsatrist";
 
-// Caminho para o certificado
-$ca_cert = "/etc/pki/tls/certs/ca-bundle.crt";
 
-// Configuração SSL
-$ssl_options = array(
-    'ssl' => array(
-        'cafile' => $ca_cert,
-        'verify_peer' => true,
-        'verify_peer_name' => true,
-    ),
-);
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname, null, MYSQLI_CLIENT_SSL);
+$conn = new mysqli($servername, $username, $password, $dbname, null, '/etc/pki/tls/certs/ca-bundle.crt');
 
-// Configuração do certificado
-mysqli_ssl_set($conn, null, null, $ca_cert, null, null);
 
 // Check connection
 if ($conn->connect_error) {
