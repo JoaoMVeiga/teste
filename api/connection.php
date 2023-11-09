65519df707dd3a22d6d64dbf4edfbd459ee0ddaa
João Veiga <67605388+JoaoMVeiga@users.noreply.github.com>
2023-11-09 23:35:20 +0000

    Update connection.php

--- api/connection.php
+++ api/connection.php
@@ -4,12 +4,12 @@ $username = $_ENV['PLANETSCALE_DB_USERNAME'];;
 $password = $_ENV['PLANETSCALE_DB_PASSWORD'];;
 $dbname = $_ENV['PLANETSCALE_DB'];;
 $ssl = $_ENV['PLANETSCALE_SSL_CERT_PATH'];
-$mysqli = mysqli_init();
-$mysqli->ssl_set(NULL, NULL, $ssl, NULL, NULL);
-$mysqli->real_connect($servername, $username, $password, $dbname, null);
+$conn = conn_init();
+$conn->ssl_set(NULL, NULL, $ssl, NULL, NULL);
+$conn->real_connect($servername, $username, $password, $dbname, null);
 
 
-if ($mysqli->connect_error) {
+if ($conn->connect_error) {
     echo 'not connected to the database';
 } else {
     echo "Connected successfully";
