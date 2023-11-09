bdef5236a6234372fd887eaa615b81406ef043f8
João Veiga <67605388+JoaoMVeiga@users.noreply.github.com>
2023-11-09 16:40:18 +0000

    Update js.js

--- js.js
+++ js.js
@@ -7,7 +7,7 @@ function inser(){
     dadosPost.append('texto', $("#nome").val());
     
     $.ajax({
-        url: "assets/model/modelComercial.php",
+        url: "api/modelComercial.php",
         method: "POST",
         data: dadosPost,
         cache: false,
