d9229c5e5ad8356a2bc9f09ce080cd3006ef0c71
João Veiga <67605388+JoaoMVeiga@users.noreply.github.com>
2023-11-09 16:59:59 +0000

    Update connection.php

--- api/connection.php
+++ api/connection.php
@@ -1,21 +1,16 @@
 <?php
-$servername = "aws.connect.psdb.cloud";
-$username = "hre4whbsult1zijifvyg";
-$password = "pscale_pw_MwKASCmwCTsRHSbB7yjxAe9Cw9mjoot1SdBXiEhibbq"; //2gUeCUsOYbfjn5Iprcwo
+$servername = "us-east.connect.psdb.cloud";
+$username = "q22jl9a79rsymx497jfd";
+$password = "pscale_pw_86DHhEdyEiF8LWxAM8dPmNcpo4kByYGSJZXLhkHmGkA"; //2gUeCUsOYbfjn5Iprcwo
 $dbname = "mariananailsatrist";
 //2gUeCUsOYbfjn5Iprcwo
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname, null, MYSQLI_CLIENT_SSL);
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-?>
 
-DB_HOST=
-DB_USERNAME=05nsos54qd07qfv8b0qg
-DB_PASSWORD=************
-DB_NAME=mariananailsatrist
-  
+?>
