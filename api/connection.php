988ee7735cd2bdd6c0e3630776865e7d9b2ee453
João Veiga <67605388+JoaoMVeiga@users.noreply.github.com>
2023-11-09 22:59:02 +0000

    Update connection.php

--- api/connection.php
+++ api/connection.php
@@ -3,26 +3,16 @@ $servername = "aws.connect.psdb.cloud";
 $username = "x47dpkdo8h444vcllq6e";
 $password = "pscale_pw_Sbwpx17SXysWnInCjDzvRhCEf8nOoCZZznT6SiMAYjZ";
 $dbname = "mariananailsatrist";
+$ssl = $_ENV['PLANETSCALE_SSL_CERT_PATH'];
+$mysqli = mysqli_init();
+$mysqli->ssl_set(NULL, NULL, $ssl, NULL, NULL);
+$mysqli->real_connect($servername, $username, $password, $dbname, null);
 
-// Criar conexão
-$conn = new mysqli($servername, $username, $password, $dbname, null, null);
 
-// Verificar conexão
-if ($conn->connect_error) {
-    die("Falha na conexão: " . $conn->connect_error);
+if ($mysqli->connect_error) {
+    echo 'not connected to the database';
+} else {
+    echo "Connected successfully";
 }
-
-// Adicionar variável de sistema PLANETSCALE_SSL_CERT_PATH ao caminho do certificado SSL
-$ssl_cert_path = getenv('PLANETSCALE_SSL_CERT_PATH');
-if ($ssl_cert_path) {
-    $conn->ssl_set(null, null, $ssl_cert_path, null, null);
-}
-
-// Verificar se a configuração SSL foi bem-sucedida
-if (!$conn->real_connect($servername, $username, $password, $dbname)) {
-    die("Falha na conexão: " . $conn->connect_error);
-}
-
-// Restante do seu código...
 
 ?>
