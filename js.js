ec332e13fd00c27317dae48673d621e193a5b439
João Veiga <67605388+JoaoMVeiga@users.noreply.github.com>
2023-11-09 23:47:44 +0000

    Update js.js

--- js.js
+++ js.js
@@ -1,5 +1,37 @@
 
-function inser(){
+function ler(){
+    
+let dadosPost = new FormData();
+
+dadosPost.append('op', 1);
+dadosPost.append('texto', $("#nome").val());
+
+$.ajax({
+    url: "api/modelComercial.php",
+    method: "POST",
+    data: dadosPost,
+    cache: false,
+    processData: false,
+    contentType: false,
+    dataType: "html",
+    success: function(response) {
+        // Manipular a resposta de sucesso aqui
+        console.log("Resposta bem-sucedida:", response);
+        
+        // Assuming you want to display the updated data, you can parse the JSON
+        console.log(jsonData);
+
+        $("#dados").html(jsonData)
+    },
+    error: function(xhr, status, error) {
+        // Manipular o erro aqui
+        console.log("Erro na solicitação AJAX:", status, error);
+    }
+});
+
+}
+
+function inserir(){
     
 let dadosPost = new FormData();
 
