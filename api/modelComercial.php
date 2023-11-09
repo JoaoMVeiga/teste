e4bfe67fc679eff239ec102601b974dc19744c15
João Veiga <67605388+JoaoMVeiga@users.noreply.github.com>
2023-11-09 23:50:00 +0000

    Update modelComercial.php

--- api/modelComercial.php
+++ api/modelComercial.php
@@ -6,5 +6,10 @@ if($_POST['op'] == 1){
     $getClientes = new Comercial();
     $resp = $getClientes -> setClientes();
     echo($resp);
+}else if($_POST['op'] == 2){
+    $inserUser = new Comercial();
+    $resp = $inserUser -> inserUser($_POST['nome']);
+    echo($resp);
 }
+    
 ?>
