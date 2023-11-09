afeb2815c100e68fd0eac23de8d71c6086521243
João Veiga <67605388+JoaoMVeiga@users.noreply.github.com>
2023-11-09 23:02:38 +0000

    Update connection.php

--- api/connection.php
+++ api/connection.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "aws.connect.psdb.cloud";
-$username = "x47dpkdo8h444vcllq6e";
-$password = "pscale_pw_Sbwpx17SXysWnInCjDzvRhCEf8nOoCZZznT6SiMAYjZ";
-$dbname = "mariananailsatrist";
+$servername = $_ENV['PLANETSCALE_DB_HOST'];;
+$username = $_ENV['PLANETSCALE_DB_USERNAME'];;
+$password = $_ENV['PLANETSCALE_DB_PASSWORD'];;
+$dbname = $_ENV['PLANETSCALE_DB'];;
 $ssl = $_ENV['PLANETSCALE_SSL_CERT_PATH'];
 $mysqli = mysqli_init();
 $mysqli->ssl_set(NULL, NULL, $ssl, NULL, NULL);
