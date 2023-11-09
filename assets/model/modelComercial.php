37f6e2096154ad760ca08d28d25087986efdc655
João Veiga <67605388+JoaoMVeiga@users.noreply.github.com>
2023-11-09 15:53:15 +0000

    Delete assets directory

--- assets/model/modelComercial.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once '../controller/controllerComercial.php';
-
-if($_POST['op'] == 1){
-    $getClientes = new Comercial();
-    $resp = $getClientes -> getClientes($_POST['texto']);
-    echo($resp);
-}
