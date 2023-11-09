d199f08d9e65042b97e3a55fdc5dbc742dcd2ae2
João Veiga <67605388+JoaoMVeiga@users.noreply.github.com>
2023-11-09 22:50:43 +0000

    Update connection.php

--- api/connection.php
+++ api/connection.php
@@ -4,18 +4,22 @@ $username = "x47dpkdo8h444vcllq6e";
 $password = "pscale_pw_Sbwpx17SXysWnInCjDzvRhCEf8nOoCZZznT6SiMAYjZ";
 $dbname = "mariananailsatrist";
 
-echo"2";
-
-
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname, null, '/etc/pki/tls/certs/ca-bundle.crt');
-
+$conn = new mysqli($servername, $username, $password, $dbname, null, null);
 
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Resto do seu código...
+// Additional SSL/TLS configuration
+$conn->ssl_set(null, null, '/etc/pki/tls/certs/ca-bundle.crt', null, null);
+
+// Check if SSL configuration is successful
+if (!$conn->real_connect($servername, $username, $password, $dbname)) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Rest of your code...
 
 ?>
