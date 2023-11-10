f8875b21384c483e4e71bd3b440c3437889d0f8e
João Veiga <67605388+JoaoMVeiga@users.noreply.github.com>
2023-11-10 10:51:19 +0000

    Update connection.php

--- api/connection.php
+++ api/connection.php
@@ -1,8 +1,8 @@
 <?php
-$servername = $_ENV['PLANETSCALE_DB_HOST'];;
-$username = $_ENV['PLANETSCALE_DB_USERNAME'];;
-$password = $_ENV['PLANETSCALE_DB_PASSWORD'];;
-$dbname = $_ENV['PLANETSCALE_DB'];;
+$servername = $_ENV['PLANETSCALE_DB_HOST'];
+$username = $_ENV['PLANETSCALE_DB_USERNAME'];
+$password = $_ENV['PLANETSCALE_DB_PASSWORD'];
+$dbname = $_ENV['PLANETSCALE_DB'];
 $ssl = $_ENV['PLANETSCALE_SSL_CERT_PATH'];
 $conn = mysqli_init();
 $conn->ssl_set(NULL, NULL, $ssl, NULL, NULL);
